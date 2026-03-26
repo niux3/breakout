0838b01234c5e1006231ade64dd6bd97460877d8
niux3 <renaudbourdeau@gmail.com>
2026-03-26 15:31:54 +0100

    feat: upgrade method load texte and sound and upgrade position sprite

--- src/sprites/paddle_sprite.py
+++ src/sprites/paddle_sprite.py
@@ -5,6 +5,6 @@ class PaddleSprite(arcade.Sprite):
     def __init__(self, texture: arcade.Texture, scale: float = 1.0) -> None:
         super().__init__(texture, scale)
 
-    def set_position(self, x: int, y: int) -> None:
+    def set_position(self, x: float, y: float) -> None:
         self.center_x = x
         self.center_y = y
--- src/views/base_view.py
+++ src/views/base_view.py
@@ -12,8 +12,16 @@ class BaseView(arcade.View):
             Config.setup_resource()
             BaseView._resources_initialized = True
 
-    def load_texture(self, path: str) -> arcade.Texture:
-        return arcade.load_texture(Config.get_asset(path))
+    def load_texture(self, path: str) -> arcade.Texture | None:
+        try:
+            return arcade.load_texture(Config.get_asset(path))
+        except FileNotFoundError:
+            print(f"Texture not found: {path}")
+            return None
 
-    def load_soud(self, path: str) -> arcade.Sound:
-        return arcade.load_sound(Config.get_asset(path))
+    def load_sound(self, path: str) -> arcade.Sound | None:
+        try:
+            return arcade.load_sound(Config.get_asset(path))
+        except FileNotFoundError:
+            print(f"Texture not found: {path}")
+            return None
