43c40861f5efc3d6b08e1ff311dadcc78c53fef3
niux3 <renaudbourdeau@gmail.com>
2026-03-26 14:00:34 +0100

    feat: display paddle

--- src/sprites/paddle_sprite.py
+++ src/sprites/paddle_sprite.py
@@ -1,6 +1,9 @@
 import arcade
+from src.config import Config
 
 
 class PaddleSprite(arcade.Sprite):
-    def __init(self) -> None:
-        super().__init__()
+    def __init__(self, texture: arcade.Texture, scale: float = 1.0) -> None:
+        super().__init__(texture, scale)
+        self.center_x = Config.WIDTH / 2
+        self.center_y = 100
--- src/views/game_view.py
+++ src/views/game_view.py
@@ -1,10 +1,24 @@
+from typing import Optional
+import arcade
+from src.sprites.paddle_sprite import PaddleSprite
 from src.views.base_view import BaseView
 
 
 class GameView(BaseView):
     def __init__(self) -> None:
         super().__init__()
+        self.all_sprites_list = arcade.SpriteList()
+
+        self.paddle_sprite: Optional[PaddleSprite] = None
+
+        self.setup()
+
+    def setup(self) -> None:
         paddle_texture = self.load_texture('images/paddle.png')
+        self.paddle_sprite = PaddleSprite(paddle_texture)
+
+        self.all_sprites_list.append(self.paddle_sprite)
 
     def on_draw(self) -> None:
         self.clear()
+        self.all_sprites_list.draw()
